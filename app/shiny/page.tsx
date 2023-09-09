ce8ab04d8303235c0c1a22e10934ad72bbf5e3b1
FranJF]] <franjcfernandez@gmail.com>
2023-09-09 21:57:30 +0200

    fix: icon

--- app/shiny/page.tsx
+++ app/shiny/page.tsx
@@ -8,7 +8,7 @@ export default async function ShinyPage() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Es shiny?</h1>
+        <h1 className={title()}>¿Es shiny?</h1>
       </div>
 
       <AutoCompleteInput data={data} />
