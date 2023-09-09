2baf79cf9344e9149728000d6b2a0739b334e650
FranJF]] <franjcfernandez@gmail.com>
2023-09-09 21:44:03 +0200

    fix: config

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,28 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
-    return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-lg text-center justify-center">
-                <h1 className={title()}>Poke&nbsp;</h1>
-                <h1 className={title({ color: "violet" })}>Go&nbsp;</h1>
-                <br />
-                <h2 className={subtitle({ class: "mt-4" })}>
-                    Ayudando a la comunidad de Pokémon Go
-                </h2>
-            </div>
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Poke&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Go&nbsp;</h1>
+        <br />
+        <h2 className={subtitle({ class: "mt-4" })}>
+          {siteConfig.description}
+        </h2>
+      </div>
 
-            <div className="flex gap-3">
-                <Link
-                    isExternal
-                    as={NextLink}
-                    className={buttonStyles({ variant: "bordered", radius: "full" })}
-                    href={siteConfig.links.github}
-                >
-                    <GithubIcon size={20} />
-                    GitHub
-                </Link>
-            </div>
-        </section>
-    );
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          as={NextLink}
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+    </section>
+  );
 }
